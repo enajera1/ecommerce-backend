ec6317e2fca6f7be0cc19a4c1e74162530a3c2f5
enajera1 <eriknajera013@gmail.com>
2022-06-18 13:55:05 -0500

    category routes updated

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection.js');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -33,6 +33,13 @@ Product.init(
       set: 10,
       validate: {
         isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
       }
     }
   },
